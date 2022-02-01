1b8d4f7eff4073bc0755f7d04c6f6ee6b684b97f
Aachal Shrestha <aachal.shrestha@student.ehb.be>
2022-02-01 11:04:53 +0100

    desktop artist page begin 01/02/2022 11:04

--- js/index.js
+++ js/index.js
@@ -1,46 +1,99 @@
 "use strict";
 
-function shiftLeft() {
-    const boxes = document.querySelectorAll(".box");
-    const tmpNode = boxes[0];
-    boxes[0].className = "box move-out-from-left";
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
 
-    setTimeout(function () {
-        if (boxes.length > 5) {
-            tmpNode.classList.add("box--hide");
-            boxes[5].className = "box move-to-position5-from-left";
+class Carousel {
+    constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    // Update css classes for gallery
+    updateGallery() {
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i+1}`);
+        });
+    }
+
+    // Update the current order of the carouselArray and gallery
+    setCurrentState(direction) {
+
+        if (direction.className == 'gallery-controls-previous') {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+            this.carouselArray.push(this.carouselArray.shift());
         }
-        boxes[1].className = "box move-to-position1-from-left";
-        boxes[2].className = "box move-to-position2-from-left";
-        boxes[3].className = "box move-to-position3-from-left";
-        boxes[4].className = "box move-to-position4-from-left";
-        boxes[0].remove();
 
-        document.querySelector(".cards__container").appendChild(tmpNode);
+        this.updateGallery();
+    }
 
-    }, 500);
+    // Construct the carousel navigation
+    // setNav() {
+    // galleryContainer.appendChild(document.createElement('ul')).className = 'gallery-nav';
 
+    // this.carouselArray.forEach(item => {
+    //   const nav = galleryContainer.lastElementChild;
+    //   nav.appendChild(document.createElement('li'));
+    // }); 
+    // }s
+
+    // Construct the carousel controls
+    setControls() {
+        this.carouselControls.forEach(control => {
+            galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+
+            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        });
+    }
+
+    // Add a click event listener to trigger setCurrentState method to rearrange carousel
+    useControls() {
+        const triggers = [...galleryControlsContainer.childNodes];
+
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+
+                if (control.className == 'gallery-controls-add') {
+                    const newItem = document.createElement('img');
+                    const latestItem = this.carouselArray.length;
+                    const latestIndex = this.carouselArray.findIndex(item => item.getAttribute('data-index') == this.carouselArray.length) + 1;
+
+                    // Assign the necessary properties for new gallery item
+                    Object.assign(newItem, {
+                        className: 'gallery-item',
+                        src: `/images/mobile-?text=${this.carouselArray.length+1}`
+                    });
+                    newItem.setAttribute('data-index', this.carouselArray.length + 1);
+
+                    // Then add it to the carouselArray and update the gallery
+                    this.carouselArray.splice(latestIndex, 0, newItem);
+                    document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
+                    this.updateGallery();
+
+                } else {
+                    this.setCurrentState(control);
+                }
+
+            });
+        });
+    }
 }
 
-function shiftRight() {
-    const boxes = document.querySelectorAll(".box");
-    boxes[4].className = "box move-out-from-right";
-    setTimeout(function () {
-        const noOfCards = boxes.length;
-        if (noOfCards > 4) {
-            boxes[4].className = "box box--hide";
-        }
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
 
-        const tmpNode = boxes[noOfCards - 1];
-        tmpNode.classList.remove("box--hide");
-        boxes[noOfCards - 1].remove();
-        let parentObj = document.querySelector(".cards__container");
-        parentObj.insertBefore(tmpNode, parentObj.firstChild);
-        tmpNode.className = "box move-to-position1-from-right";
-        boxes[0].className = "box move-to-position2-from-right";
-        boxes[1].className = "box move-to-position3-from-right";
-        boxes[2].className = "box move-to-position4-from-right";
-        boxes[3].className = "box move-to-position5-from-right";
-    }, 500);
-
-}
+exampleCarousel.setControls();
+// exampleCarousel.setNav();
+exampleCarousel.useControls();
